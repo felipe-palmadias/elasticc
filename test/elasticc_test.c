564dd14a28c6d31d67ca5ee9203d8918176adea8
felipe-palmadias <felipe.palmadias@axur.com>
2024-02-23 14:24:19 -0300

    Improve initialization test

--- test/elasticc_test.c
+++ test/elasticc_test.c
@@ -1,8 +1,29 @@
 #include <criterion/criterion.h>
 #include "../src/elasticc.h"
+#include "../src/util.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void assert_that_the_cluster_data_file_was_created() {
+	char *home = getenv("HOME");
+	char *file_path = xmalloc(sizeof(home) + sizeof("/.elasticc/cluster.dat") + 1);
+	strcpy(file_path, home);
+	strcat(file_path, "/.elasticc/cluster.dat");
+	FILE *fp = fopen(file_path, "r");
+	cr_assert(fp != NULL, "Cluster data file not found");
+
+	// finally
+	fclose(fp);
+	free(file_path);
+}
 
 Test(elasticc, initialize_library) {
 	int result = elasticc_init();
 
 	cr_assert(result == 0, "initialization failure");
+	assert_that_the_cluster_data_file_was_created();
 }
+
+
+
